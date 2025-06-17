7f3b696be03efc7e9faeea21eba9488389e9e6f6
Changhyun-Kyle <changhyun.kyle@gmail.com>
2025-06-17 13:36:17 +0900

    config: 프로젝트 초기 세팅

---
+++ Fastlane-Example/Fastlane-Example/App/SceneDelegate.swift
@@ -0,0 +1,36 @@
+//
+//  SceneDelegate.swift
+//  Fastlane-Example
+//
+//  Created by 아티젠스페이스 on 6/17/25.
+//
+
+import UIKit
+
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+    
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) { 
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        self.window = UIWindow(windowScene: windowScene)
+        
+        let viewController = ViewController()
+        self.window?.rootViewController = viewController
+        self.window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) { }
+
+    func sceneDidBecomeActive(_ scene: UIScene) { }
+
+    func sceneWillResignActive(_ scene: UIScene) { }
+
+    func sceneWillEnterForeground(_ scene: UIScene) { }
+
+    func sceneDidEnterBackground(_ scene: UIScene) { }
+}
---
+++ Fastlane-Example/Fastlane-Example/View/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  Fastlane-Example
+//
+//  Created by 아티젠스페이스 on 6/17/25.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
